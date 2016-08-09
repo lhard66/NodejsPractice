143b0fbdd3b961e2d2be53f155357e952deb1c4b
LiuTianqing <lhard@qq.com>
2016-08-09 11:43:02 +0800

    complete to post request get data

--- P_Express/p_base.js
+++ P_Express/p_base.js
@@ -3,6 +3,9 @@
  */
 var http = require('http');
 var express = require('express');
+var bodyparser=require('body-parser');
+
+var fs = require('fs');
 
 var app = express();
 
@@ -23,12 +26,27 @@ app.get('/student/:id/:name', (req, res)=> {
     }
 });
 
-//！！！注意此处post请求失败！！！
 //post请求
-app.set('view engine','ejs');//设置模版引擎
-app.post('/',(req,res)=> {
-    console.log('post');
-    res.send('ths is post');
+app.set('view engine', 'ejs');//设置模版引擎
+
+app.get('/form', (req, res)=> {
+    fs.readFile('./p_base.ejs', (err, data)=> {
+        if (err) {
+            res.send('error');
+        }
+        res.send(data.toString());
+    });
+});
+
+//用于接收类型为www-form-urlencoded的post请求的Server
+app.use(bodyparser.urlencoded({
+    extended:true
+}));
+app.post('/form', (req, res)=> {
+    var body = req.body;
+    console.log(body);
+    res.send(body.name);
+    //res.send('ths is post,name is ' + name);
 });
 app.listen(3000);
 
