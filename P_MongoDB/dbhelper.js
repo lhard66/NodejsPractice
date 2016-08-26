2462ef6aa23b37e062d30cb2b46807af09de939b
LiuTianqing <lhard@qq.com>
2016-08-26 17:11:43 +0800

    add dbhelper.js

---
+++ P_MongoDB/dbhelper.js
@@ -0,0 +1,93 @@
+var mongoClient = require('mongodb').MongoClient;
+
+function _connectDB(callback) {
+    var dburl = 'mongodb://localhost:27017/test';
+    mongoClient.connect(dburl, (err, db)=> {
+        callback(err, db);
+    });
+}
+//插入一个或多个
+exports.insertDocuments = function (collectionName, json, callback) {
+    _connectDB((err, db)=> {
+        if (err) {
+            db.close();
+            console.log('inserDocuments err');
+            return;
+        }
+        var collection = db.collection(collectionName);
+        collection.insertMany(json, (err, result)=> {
+            callback(err, result);
+            db.close();
+        });
+    });
+}
+//只删除一个
+exports.deleteOne = function (collectionName, json, callback) {
+    _connectDB((err, db)=> {
+        if (err) {
+            db.close();
+            console.log('deleteOne err');
+            return;
+        }
+        var collection = db.collection(collectionName);
+        collection.deleteOne(json, (err, result)=> {
+            callback(err, result);
+            db.close();
+        });
+    })
+}
+//删除多个
+exports.deleteMany = function (collectionName, json, callback) {
+    _connectDB((err, db)=> {
+        if (err) {
+            db.close();
+            console.log('deleteMany err');
+            return;
+        }
+        var collection = db.collection(collectionName);
+        collection.deleteMany(json, (err, result)=> {
+            callback(err, result);
+            db.close();
+        });
+    });
+}
+//更新一个或多个
+exports.updateMany = function (collectionName, oldjson, newjson, optionjson, callback) {
+    _connectDB((err, db)=> {
+        if (err) {
+            db.close();
+            console.log('updateMany err');
+            return;
+        }
+        var collection = db.collection(collectionName);
+        collection.updateMany(oldjson, {$set: newjson}, optionjson, (err, result)=> {
+            callback(err, result);
+            db.close();
+        });
+    });
+}
+//查找,{}为查找全部
+exports.find = function (collectionName, queryjson, callback) {
+    _connectDB((err, db)=> {
+        if (err) {
+            db.close();
+            console.log('findAll err');
+            return;
+        }
+        var collection = db.collection(collectionName);
+        collection.find(queryjson).toArray((err, docs)=> {
+            callback(err, docs);
+            db.close();
+        });
+    });
+}
+
+exports.test = function () {
+    _connectDB((err, db)=> {
+        if (err) {
+            console.log('test_err');
+        }
+        console.log(db);
+        db.close();
+    });
+}
---
+++ P_MongoDB/p_dbhelperTest.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var dbhelper = require('./dbhelper');
+
+var app = express();
+
+
+app.get('/', (req, res)=> {
+    dbhelper.test()
+    res.send('connect success!');
+});
+
+app.get('/add', (req, res)=> {
+    dbhelper.insertDocuments('teacher', [
+        {a: 1, name: "jim"}, {b: 2, name: "lucy"}, {c: 3, name: "tom"}
+    ], (err, result)=> {
+        if (err) {
+            res.send('add err');
+            return;
+        }
+        res.send(result);
+    });
+});
+
+app.get('/del', (req, res)=> {
+    dbhelper.deleteOne('teacher', {borough: "Bronx"}, (err, result)=> {
+        if (err) {
+            res.send('/del err');
+            return;
+        }
+        res.send(result);
+    });
+});
+
+app.get('/delmany', (req, res)=> {
+    dbhelper.deleteMany('teacher', {c: 3}, (err, result)=> {
+        if (err) {
+            res.send('delMany err');
+            return;
+        }
+        res.send(result);
+    });
+});
+
+app.get('/update', (req, res)=> {
+    dbhelper.updateMany('teacher', {a: 1}, {name: "lhard"}, {upsert: true}, (err, result)=> {
+        if (err) {
+            console.log('update err');
+            return;
+        }
+        res.send(result);
+    });
+});
+
+app.get('/findAll', (req, res)=> {
+    dbhelper.find('teacher', {}, (err, docs)=> {
+        if (err) {
+            console.log('findall err');
+            return;
+        }
+        res.send(docs);
+    });
+});
+
+app.get('/findid', (req, res)=> {
+    dbhelper.find('teacher', {name: 'lhard'}, (err, docs)=> {
+        if (err) {
+            console.log('findid err');
+            return;
+        }
+        res.send(docs);
+    });
+});
+app.listen(3000);
