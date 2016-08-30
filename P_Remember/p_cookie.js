18a5ba56709a422e3e7efedf918a44d6bb539e23
LiuTianqing <lhard@qq.com>
2016-08-30 17:12:57 +0800

    study cookie in express frame

---
+++ P_Remember/p_cookie.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+//cookie要使用此中间件
+app.use(cookieParser());
+
+app.get('/city', (req, res)=> {
+    //获得url参数
+    var name = req.query.name;
+    //得到cookie信息
+    var footpoint = (req.cookies.city || '起始地') + '-->' + name;
+
+    //var footpoint = footpoint + '-->' + name;
+    res.cookie('city', footpoint, {maxAge: 10000, httpOnly: true});
+    res.send('this city name:' + name);
+});
+
+app.get('/', (req, res)=> {
+    var footpoint = req.cookies.city;
+    console.log(footpoint);
+    res.send(footpoint);
+});
+
+app.listen(3000);
