bd83e33d138afee088a9750032780e0c8eb0171a
Hero <lhard@qq.com>
2017-02-03 17:41:13 +0800

    send msg to everyone,include sender

--- P_WebSocket/index.js
+++ P_WebSocket/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app=express();
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
@@ -11,7 +11,15 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+  // console.log('a user connected');
+  // socket.on('disconnect',function(){
+  // 	console.log('user disconnected');
+  // });
+
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  })
 });
 
 http.listen(3000, function() {
